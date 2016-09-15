7c29e6fc327717fc296397530439d8a7a8b6212b
Max Aller <nanodeath@gmail.com>
2016-09-14 20:25:57 -0700

    Part 6, adding an input adapter and creating boxes on click.

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -4,14 +4,18 @@ import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.physics.box2d.BodyDef
 import com.badlogic.gdx.physics.box2d.PolygonShape
 import com.badlogic.gdx.physics.box2d.World
 import com.google.inject.Guice
+import com.google.inject.Inject
 import com.google.inject.Injector
 
 class MyGdxGame : ApplicationAdapter() {
@@ -29,6 +33,8 @@ class MyGdxGame : ApplicationAdapter() {
         }
 
         createEntities()
+
+        Gdx.input.inputProcessor = injector.getInstance(MyInputAdapter::class.java)
     }
 
     private fun createEntities() {
@@ -46,6 +52,18 @@ class MyGdxGame : ApplicationAdapter() {
             body.setTransform(transform.position, 0F)
             add(PhysicsComponent(body))
         })
+        engine.addEntity(Entity().apply {
+            add(TransformComponent(Vector2(0F, 0F)))
+
+            val body = world.createBody(BodyDef().apply {
+                type = BodyDef.BodyType.StaticBody
+            })
+            body.createFixture(PolygonShape().apply {
+                setAsBox(15F, 1F)
+            }, 1.0F)
+            body.setTransform(transform.position, 0F)
+            add(PhysicsComponent(body))
+        })
     }
 
     override fun render() {
@@ -57,6 +75,29 @@ class MyGdxGame : ApplicationAdapter() {
     override fun dispose() {
         batch.dispose()
         img.dispose()
+    }
+}
+
+class MyInputAdapter @Inject constructor(private val camera: OrthographicCamera,
+                                         private val engine: Engine,
+                                         private val world: World) : InputAdapter() {
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        val worldPosition = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0F))
+
+        engine.addEntity(Entity().apply {
+            add(TransformComponent(Vector2(worldPosition.x, worldPosition.y)))
+
+            val body = world.createBody(BodyDef().apply {
+                type = BodyDef.BodyType.DynamicBody
+            })
+            body.createFixture(PolygonShape().apply {
+                setAsBox(1F, 1F)
+            }, 1.0F)
+            body.setTransform(transform.position, 0F)
+            add(PhysicsComponent(body))
+        })
+
+        return true
     }
 }
 
